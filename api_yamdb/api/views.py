39406bd228164a1a18e4257015c4ad0785472787
Mamrenko-Alex <Mamrenkodev@yandex.com>
2022-08-11 20:26:02 +0200

    War in Isort =)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -49,7 +49,7 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return user
+        return 'Это сообщение пользователь никогда не увидит!'
 
 
 @api_view(['POST'])
@@ -81,7 +81,7 @@ def sign_up(requset):
             [f'{email}'],
         )
         return Response(serializers.data, status=status.HTTP_200_OK)
-    return 'Test flake8'
+    return 'Это сообщение пользователь никогда не увидит!'
 
 
 @api_view(['POST'])
