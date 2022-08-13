423eeb5fab282869c0e9d7b5cbf949a3703bd42b
Mamrenko-Alex <Mamrenkodev@yandex.com>
2022-08-13 12:48:01 +0200

    Update settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '51.250.98.88',
