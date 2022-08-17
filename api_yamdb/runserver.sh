b8a183183f051d75415adf119d6b7c172e8f01ab
Mamrenko-Alex <Mamrenkodev@yandex.com>
2022-08-17 21:49:52 +0200

    Test

--- api_yamdb/runserver.sh
+++ api_yamdb/runserver.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py loaddata fixtures.json
 python manage.py collectstatic --no-input
-sudo systemctl start gunicorn
+gunicorn api_yamdb.wsgi:application --bind 0:8000
